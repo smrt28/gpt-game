4a1cc952a458e034b6e75385803f67b31c824fcc
Ondrej Holecek <ondrej@vivaldi.com>
2025-09-06 21:20:07 +0200

    home component

--- frontend/src/apphome_component.rs
+++ frontend/src/apphome_component.rs
@@ -10,70 +10,53 @@ use crate::Route;
 
 #[function_component(AppHome)]
 pub fn app_home() -> Html {
-    let navigate = use_navigator().expect("Must be used within a Router");
-    let navigate2 = navigate.clone();
-
+    let navigator = use_navigator().expect("Must be used within a Router");
     let render_trigger = use_state(|| 0);
-    let on_new_game = {
+
+    // Helper function to create new game and navigate
+    let create_game_and_navigate = |route: Route| {
+        let navigator = navigator.clone();
         Callback::from(move |_| {
-            let new_game_navigate = navigate.clone();
+            let navigator = navigator.clone();
+            let route = route.clone();
             spawn_local(async move {
-                info!("home-new-game {}!", get_current_language().to_code());
+                info!("Creating new game, lang={}", get_current_language().to_code());
                 match fetch_new_game_token().await {
                     Ok(new_token) => {
-                        LocalStorage::set("token", &new_token).and_then(|()| {
-                            new_game_navigate.push(&Route::Game);
-                            Ok(())
-                        }).ok();
+                        if LocalStorage::set("token", &new_token).is_ok() {
+                            navigator.push(&route);
+                        } else {
+                            log::error!("Failed to save token to localStorage");
+                            navigator.push(&Route::Error);
+                        }
                     }
-                    Err(_e) => {
-
+                    Err(e) => {
+                        log::error!("Failed to create new game: {e:?}");
+                        navigator.push(&Route::Error);
                     }
                 }
             });
         })
     };
 
-
-    let on_new_custom_game = {
-        Callback::from(move |_| {
-            let new_game_navigate = navigate2.clone();
-            spawn_local(async move {
-                info!("home-new-game {}!", get_current_language().to_code());
-                match fetch_new_game_token().await {
-                    Ok(new_token) => {
-                        LocalStorage::set("token", &new_token).and_then(|()| {
-                            new_game_navigate.push(&Route::CustomGameDesign);
-                            Ok(())
-                        }).ok();
-                    }
-                    Err(_e) => {
-
-                    }
-                }
-            });
-        })
-    };
-
-
+    let on_new_game = create_game_and_navigate(Route::Game);
+    let on_new_custom_game = create_game_and_navigate(Route::CustomGameDesign);
 
     let on_language_changed = {
         let render_trigger = render_trigger.clone();
         Callback::from(move |_| {
             render_trigger.set(*render_trigger + 1);
-            info!("language-changed");
         })
     };
 
     html! {
         <>
-        <h1>{ t("ui.page_title") }</h1>
-        <button class="new-game" onclick={on_new_game}>{ t("ui.new_game") }</button>
-        <button class="new-game" onclick={on_new_custom_game}>{ t("ui.new_custom_game") }</button>
-        <div class="language-bar">
+            <h1>{ t("ui.page_title") }</h1>
+            <button class="new-game" onclick={on_new_game}>{ t("ui.new_game") }</button>
+            <button class="new-game" onclick={on_new_custom_game}>{ t("ui.new_custom_game") }</button>
+            <div class="language-bar">
                 <LanguageSelector on_language_changed={Some(on_language_changed)} />
-        </div>
+            </div>
         </>
-
     }
 }
--- frontend/src/locale.rs
+++ frontend/src/locale.rs
@@ -39,7 +39,7 @@ impl LocaleManager {
         // Page titles and headers
         strings.insert("ui.page_title".to_string(), "Guess Who".to_string());
         strings.insert("ui.game_header".to_string(), "Guess Who".to_string());
-        strings.insert("ui.new_game".to_string(), "New game".to_string());
+        strings.insert("ui.new_game".to_string(), "New game, random identity".to_string());
         strings.insert("ui.new_custom_game".to_string(), "Game for friend".to_string());
         strings.insert("ui.custom_game_design".to_string(), "Custom game builder".to_string());
 
@@ -90,7 +90,7 @@ impl LocaleManager {
         // Page titles and headers
         strings.insert("ui.page_title".to_string(), "Hádej kdo jsem".to_string());
         strings.insert("ui.game_header".to_string(), "Hádej kdo jsem".to_string());
-        strings.insert("ui.new_game".to_string(), "Nová hra".to_string());
+        strings.insert("ui.new_game".to_string(), "Nová hra, náhodná identita".to_string());
         strings.insert("ui.new_custom_game".to_string(), "Hra pro přítele".to_string());
         strings.insert("ui.404".to_string(), "404".to_string());
         strings.insert("ui.server_error".to_string(), "Chyba serveru".to_string());
